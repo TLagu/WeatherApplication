0bb5c78e33f998fc01f630efa86df876a3d3dd28
Tomasz Łagowski <tomasz.lagowski@lagu.pl>
2022-02-27 14:45:27 +0100

    Adding new locality

--- src/main/java/com/sda/weather/Application.java
+++ src/main/java/com/sda/weather/Application.java
@@ -13,7 +13,6 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Michal tu byl");
         StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure()
                 .build();
